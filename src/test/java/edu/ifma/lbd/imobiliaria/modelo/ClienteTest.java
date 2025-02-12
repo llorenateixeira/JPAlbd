3c2f414ce90eaab913c3d838140a7003657d4f25
diogoagguiar <diogoaguiar@acad.ifma.edu.br>
2025-02-12 15:33:59 -0300

    modificando os arquivos

---
+++ src/test/java/edu/ifma/lbd/imobiliaria/modelo/ClienteTest.java
@@ -0,0 +1,17 @@
+package edu.ifma.lbd.imobiliaria.modelo;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+
+
+
+    @Test
+    public void getTelefones() {
+    }
+
+    @Test
+    public void setTelefones() {
+    }
+}
---
+++ src/test/java/edu/ifma/lbd/imobiliaria/modelo/builder/ClienteBuilder.java
@@ -0,0 +1,41 @@
+package edu.ifma.lbd.imobiliaria.modelo.builder;
+
+import java.util.*;
+
+public class ClienteBuilder {
+
+	private Cliente cliente;
+	
+	private ClienteBuilder() {}
+	
+	public static ClienteBuilder umCliente() {
+		
+		ClienteBuilder builder = new ClienteBuilder();
+		builder.cliente = new Cliente();
+		builder.cliente.setNome("Cliente 1");
+		return builder;
+	}
+	
+	public ClienteBuilder comNome(String nome) {
+		cliente.setNome(nome);
+		return this;
+	}
+
+
+	public ClienteBuilder possui(String ...telefones) {
+		cliente.setTelefones( new LinkedHashSet<>(Arrays.asList(telefones)));
+	    /*
+        //No Java 9+
+        Set<String> setTelefones = Set.of(telefones);
+
+        //No Java 10+, o tipo genêrico é inferido pelo parâmetro
+        var setTelefones = Set.of(telefones);
+	    */
+		return this;
+	}
+	
+	
+	public Cliente constroi(){
+		return this.cliente;
+	}
+}
